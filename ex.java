3615bb0d20f13513baa1d3cf3a6312a131099bba
nemo1991edu <edu.wolf.11@gmail.com>
2020-10-05 11:03:42 -0700

    first commit

---
+++ ex.java
@@ -0,0 +1,7 @@
+public class ex {
+    public static void main(String[] args){        
+        int birthday = 2345;
+        System.out.println(birthday);        
+        birthday = 6789098;
+        System.out.println(birthday);    }
+}
